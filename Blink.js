260c57d1b0c4a19defb9886c4fa1b88d7f177d27
march-dave <ysangy@gmail.com>
2018-06-10 21:18:03 -0400

    add style in React Native

--- Blink.js
+++ Blink.js
@@ -1,15 +1,21 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 class Blink extends Component {
   //let
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <Text>{this.props.text}</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({ 
+  container: {
+    backgroundColor: "#999"
+  }
+});
 
 export default Blink;
