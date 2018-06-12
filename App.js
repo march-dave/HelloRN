e837b3730e43bc29e68ab0735e6ab44dd83fde5b
march-dave <ysangy@gmail.com>
2018-06-12 19:19:12 -0400

    flexDirection row

--- App.js
+++ App.js
@@ -38,6 +38,7 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
