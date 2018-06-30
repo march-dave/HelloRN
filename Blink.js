141ff50f3d726b129d4e57cae4dd171beaaada55
march-dave <ysangy@gmail.com>
2018-06-29 21:22:47 -0400

    up to date

--- Blink.js
+++ Blink.js
@@ -45,6 +45,12 @@ const styles = StyleSheet.create({
     height: 50,
     // backgroundColor: "#999"
   },
+  five: {
+    // flex: 1,
+    width: 50,
+    height: 50,
+    // backgroundColor: "#999"
+  },
 });
 
 export default Blink;
